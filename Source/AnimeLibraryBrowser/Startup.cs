66f0ad68818852447bc3a1de19363a4391b5ce51
Andrey Talanin <andrey.talanin@outlook.com>
2022-03-15 00:30:01 +0300

    Create an ASP.NET Core Web API application without the client application for the moment

---
+++ Source/AnimeLibraryBrowser/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AnimeLibraryBrowser
+{
+    public class Startup
+    {
+        private const string c_clientApplicationPath = "ClientApplication";
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = $"{c_clientApplicationPath}/build";
+            });
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment webHostEnvironment)
+        {
+            if (webHostEnvironment.IsDevelopment())
+                applicationBuilder.UseDeveloperExceptionPage();
+            else
+            {
+                applicationBuilder.UseExceptionHandler("/Error");
+                applicationBuilder.UseHsts();
+            }
+
+            applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseStaticFiles();
+            applicationBuilder.UseSpaStaticFiles();
+
+            applicationBuilder.UseRouting();
+
+            applicationBuilder.UseEndpoints(endpointRouteBuilder =>
+            {
+                endpointRouteBuilder.MapControllers();
+            });
+
+            applicationBuilder.UseSpa(spaBuilder =>
+            {
+                spaBuilder.Options.SourcePath = c_clientApplicationPath;
+
+                if (webHostEnvironment.IsDevelopment())
+                {
+                    spaBuilder.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
