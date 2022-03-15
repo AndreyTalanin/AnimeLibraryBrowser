ebb54e5f316b0879c157630e11cfaea435580aeb
Andrey Talanin <andrey.talanin@outlook.com>
2022-03-15 19:20:31 +0300

    Add Web API controllers with method placeholders

---
+++ Source/AnimeLibraryBrowser/Controllers/ReleaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using AnimeLibraryBrowser.Entities;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeLibraryBrowser.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReleaseController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Release>> GetAllReleasesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{title}")]
+        public string GetReleaseAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
