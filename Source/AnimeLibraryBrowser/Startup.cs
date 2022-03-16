a1c41459953add0420ad8d0717a53d5269f761d4
Andrey Talanin <andrey.talanin@outlook.com>
2022-03-17 00:16:51 +0300

    Add the FileTypeResolver service class

--- Source/AnimeLibraryBrowser/Startup.cs
+++ Source/AnimeLibraryBrowser/Startup.cs
@@ -5,6 +5,7 @@ using AnimeLibraryBrowser.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +27,13 @@ namespace AnimeLibraryBrowser
         {
             services.Configure<AnimeLibraryConfiguration>(Configuration.GetSection(nameof(AnimeLibraryConfiguration)));
 
+            services.AddMemoryCache();
+
+            services.AddSingleton<IContentTypeProvider, FileExtensionContentTypeProvider>();
+
             services.AddSingleton<IReleaseDetailsProvider, ReleaseDetailsProvider>();
             services.AddSingleton<IReleaseDirectorySelector, ReleaseDirectorySelector>();
+            services.AddSingleton<IFileTypeResolver, FileTypeResolver>();
 
             services.AddSwaggerGen();
             services.AddControllersWithViews();
