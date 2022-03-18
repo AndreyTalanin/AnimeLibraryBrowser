a7627d718a3c42680e94b095b7421f48ad694d31
Andrey Talanin <andrey.talanin@outlook.com>
2022-03-18 19:56:59 +0300

    Rename the FileController API controller class and change its exposed methods

---
+++ Source/AnimeLibraryBrowser/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeLibraryBrowser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        [HttpGet("file")]
+        public Task<FileResult> DownloadFileAsync([FromQuery] string relativePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("directory")]
+        public Task<FileResult> DownloadDirectoryAsync([FromQuery] string relativePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
