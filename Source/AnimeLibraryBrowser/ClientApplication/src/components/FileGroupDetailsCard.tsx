6995bd20dee5f8cbf4558e503abb719e0e7a9f73
mp913 <zarkov01@mail.ru>
2022-03-24 17:41:39 +0300

    Add styled components to the FileGroupDetailsCard component

--- Source/AnimeLibraryBrowser/ClientApplication/src/components/FileGroupDetailsCard.tsx
+++ Source/AnimeLibraryBrowser/ClientApplication/src/components/FileGroupDetailsCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Button, Card, Stack, Table } from "react-bootstrap";
 import File from "../entities/File";
 import FileGroup from "../entities/FileGroup";
 import FileType from "../entities/FileType";
 import { downloadDirectoryAsync, downloadFileAsync } from "../requests/contentRequests";
+import "./FileGroupDetailsCard.css";
 
 export interface FileGroupDetailsCardProps {
   fileGroup: FileGroup;
@@ -21,38 +23,44 @@ const FileGroupDetailsCard = (props: FileGroupDetailsCardProps): JSX.Element => 
   };
 
   return (
-    <>
-      <div>
-        {props.fileGroup.name}
-        <button onClick={() => downloadFileGroup(props.fileGroup)}>Download All Files</button>
-      </div>
-      {props.advancedModeEnabled && <div>{`Relative Path: "${props.fileGroup.relativePath}"`}</div>}
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">File Name</th>
-            <th scope="col">Type</th>
-            {props.advancedModeEnabled && <th scope="col">Length</th>}
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.fileGroup.files.map((file) => (
-            <>
+    <Card border="secondary">
+      <Card.Body>
+        <Card.Title>{props.fileGroup.name}</Card.Title>
+        {props.advancedModeEnabled && <Card.Subtitle>{`Relative Directory Path: "${props.fileGroup.relativePath}"`}</Card.Subtitle>}
+        <Table striped bordered hover size="sm" style={{ marginTop: "16px" }}>
+          <thead>
+            <tr>
+              <th scope="col">File Name</th>
+              <th scope="col">Type</th>
+              {props.advancedModeEnabled && <th scope="col">Length</th>}
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.fileGroup.files.map((file) => (
               <tr>
                 <td>{file.name}</td>
-                <td>{FileType[file.type]}</td>
-                {props.advancedModeEnabled && <td>{file.length}</td>}
-                <td>
-                  <button onClick={() => downloadFile(file)}>Download File</button>
-                  <button onClick={() => copyFtpLinkToClipboard(file.ftpLink)}>Copy FTP Link</button>
+                <td className="td-file-type">{FileType[file.type]}</td>
+                {props.advancedModeEnabled && <td className="td-file-length">{file.length}</td>}
+                <td className="td-file-actions">
+                  <Stack direction="horizontal" gap={1}>
+                    <Button variant="outline-primary" size="sm" onClick={() => downloadFile(file)}>
+                      Download File
+                    </Button>
+                    <Button variant="outline-primary" size="sm" onClick={() => copyFtpLinkToClipboard(file.ftpLink)}>
+                      Copy FTP Link
+                    </Button>
+                  </Stack>
                 </td>
               </tr>
-            </>
-          ))}
-        </tbody>
-      </table>
-    </>
+            ))}
+          </tbody>
+        </Table>
+        <Button variant="primary" size="sm" onClick={() => downloadFileGroup(props.fileGroup)}>
+          Download All Files
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
